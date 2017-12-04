caeb2c97140742e71817f1149d90b77478961427
Lustre <firelustre@163.com>
2017-12-04 23:46:39 +0800

    容器网络共享，完善测试demo

--- lnmp/www/site1/index.php
+++ lnmp/www/site1/index.php
@@ -1,2 +1,22 @@
 <?php
+// test to connect redis
+$redis = new \Redis();
+try {
+    $redis->connect('redis', 6379);
+} catch (\Exception $e) {
+    var_dump($e->getMessage())  ;
+    die;
+}
+
+// succes to echo 1
+echo $redis->set('hello', 'hello docker!');
+
+// success to use mysql without PDOException
+try {
+    $connection = new PDO('mysql:host=mysql;dbname=mysql', 'root', '123456');
+} catch (PDOException $e) {
+    var_dump($e->getMessage());
+    die;
+}
+
 phpinfo();
